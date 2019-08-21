170e5281969a78a617241c7466c0eff6512c8d23
Barry Eom <31739208+barieom@users.noreply.github.com>
2019-08-21 12:18:00 +0100

    made preliminary changes. need to edit projects and portfolio now

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Barry Eom.", "a Product Manager.", "a Leader.", "a Founder."],
       typeSpeed: 100,
       loop: true,
     });
