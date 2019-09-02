7bcd1e48be204f0a002948aedf53406118a6078f
Barry Eom <31739208+barieom@users.noreply.github.com>
2019-09-02 15:59:14 +0200

    edited skills

--- js/main.js
+++ js/main.js
@@ -8,43 +8,43 @@ $(document).ready(function() {
   // ========================================================================= //
 
 
-  $(document).on("scroll", onScroll);
+  // $(document).on("scroll", onScroll);
 
-  $('a[href^="#"]').on('click', function(e) {
-    e.preventDefault();
-    $(document).off("scroll");
+  // $('a[href^="#"]').on('click', function(e) {
+  //   e.preventDefault();
+  //   $(document).off("scroll");
 
-    $('a').each(function() {
-      $(this).removeClass('active');
-      if ($(window).width() < 768) {
-        $('.nav-menu').slideUp();
-      }
-    });
+  //   $('a').each(function() {
+  //     $(this).removeClass('active');
+  //     if ($(window).width() < 768) {
+  //       $('.nav-menu').slideUp();
+  //     }
+  //   });
 
-    $(this).addClass('active');
+  //   $(this).addClass('active');
 
-    var target = this.hash,
-        menu = target;
+  //   var target = this.hash,
+  //       menu = target;
 
-    target = $(target);
-    $('html, body').stop().animate({
-      'scrollTop': target.offset().top - 80
-    }, 500, 'swing', function() {
-      window.location.hash = target.selector;
-      $(document).on("scroll", onScroll);
-    });
-  });
+  //   target = $(target);
+  //   $('html, body').stop().animate({
+  //     'scrollTop': target.offset().top - 80
+  //   }, 500, 'swing', function() {
+  //     window.location.hash = target.selector;
+  //     $(document).on("scroll", onScroll);
+  //   });
+  // });
 
 
-  function onScroll(event) {
-    if ($('.home').length) {
-      var scrollPos = $(document).scrollTop();
-      $('nav ul li a').each(function() {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-      });
-    }
-  }
+  // function onScroll(event) {
+  //   if ($('.home').length) {
+  //     var scrollPos = $(document).scrollTop();
+  //     $('nav ul li a').each(function() {
+  //       var currLink = $(this);
+  //       var refElement = $(currLink.attr("href"));
+  //     });
+  //   }
+  // }
 
   // ========================================================================= //
   //  //NAVBAR SHOW - HIDE
@@ -105,31 +105,57 @@ $(document).ready(function() {
   //  magnificPopup
   // ========================================================================= //
 
-  var magnifPopup = function() {
-    $('.popup-img').magnificPopup({
-      type: 'image',
+  // var magnifPopup = function() {
+  //   $('.popup-img').magnificPopup({
+  //     type: 'inline',
+  //     removalDelay: 300,
+  //     mainClass: 'mfp-with-zoom',
+  //     gallery: {
+  //       enabled: true
+  //     },
+  //     zoom: {
+  //       enabled: true, // By default it's false, so don't forget to enable it
+
+  //       duration: 300, // duration of the effect, in milliseconds
+  //       easing: 'ease-in-out', // CSS transition easing function
+
+  //       // The "opener" function should return the element from which popup will be zoomed in
+  //       // and to which popup will be scaled down
+  //       // By defailt it looks for an image tag:
+  //       opener: function(openerElement) {
+  //         // openerElement is the element on which popup was initialized, in this case its <a> tag
+  //         // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+  //         return openerElement.is('img') ? openerElement : openerElement.find('img');
+  //       }
+  //     }
+  //   });
+  // };
+
+  var magnifPopup = function () {
+      $('.popup-with-zoom-anim').magnificPopup({
+      type: 'inline',
+
+      fixedContentPos: true,
+      fixedBgPos: true,
+
+      overflowY: 'auto',
+
+      closeBtnInside: true,
+      preloader: false,
+      
+      midClick: true,
       removalDelay: 300,
-      mainClass: 'mfp-with-zoom',
-      gallery: {
-        enabled: true
-      },
-      zoom: {
-        enabled: true, // By default it's false, so don't forget to enable it
-
-        duration: 300, // duration of the effect, in milliseconds
-        easing: 'ease-in-out', // CSS transition easing function
-
-        // The "opener" function should return the element from which popup will be zoomed in
-        // and to which popup will be scaled down
-        // By defailt it looks for an image tag:
-        opener: function(openerElement) {
-          // openerElement is the element on which popup was initialized, in this case its <a> tag
-          // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-          return openerElement.is('img') ? openerElement : openerElement.find('img');
+      mainClass: 'my-mfp-zoom-in',
+      callbacks: {
+          open: function() {
+            jQuery('body').addClass('noscroll');
+          },
+          close: function() {
+            jQuery('body').removeClass('noscroll');
+          }
         }
-      }
     });
-  };
+  }
 
 
   // Call the functions
